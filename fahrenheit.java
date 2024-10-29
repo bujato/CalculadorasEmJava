bc656de5542dbd12b210e85c53de514dad520855
Enzo Guilherme Bujato <120108745+bujato@users.noreply.github.com>
2024-10-29 14:23:56 -0300

    Update fahrenheit.java
    Mudanças visuais do código

--- fahrenheit.java
+++ fahrenheit.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class fahrenheit {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
+        
         System.out.println("Temperatura em Celsius");
         int celsius;
         celsius = sc.nextInt();
